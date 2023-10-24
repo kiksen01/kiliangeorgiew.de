324b4ba8070185cfcaea45eaf4f582d988f84f51
Kilian Georgiew <kilian.georgiew@gmail.com>
2023-10-24 17:38:49 +0200

    added images and a slideshow

--- kiliangeorgiew/src/app/app.component.spec.ts
+++ kiliangeorgiew/src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'kiliangeorgiew'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kiliangeorgiew');
   });
 
   it('should render title', () => {
--- kiliangeorgiew/src/app/app.component.ts
+++ kiliangeorgiew/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kiliangeorgiew';
+export class AppComponent implements OnInit {
+  images = ['coding.jpg','macbook.jpg','office.jpg']
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage() {
+    
+    setInterval(() =>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() =>{
+        this.showImage = true;
+      }, 1);
+    },8000)
+    
+  }
+
+  
 }
